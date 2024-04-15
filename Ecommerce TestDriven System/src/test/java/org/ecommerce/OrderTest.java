62c7b4e2c6f908226b950c96e891edc7a97fde20
youssefalbertt <youssefalbert2003@gmail.com>
2024-04-15 17:14:09 +0200

    add order test file

---
+++ Ecommerce TestDriven System/src/test/java/org/ecommerce/OrderTest.java
@@ -0,0 +1,66 @@
+package org.ecommerce;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderTest {
+
+    @Test
+    public void testConstructor() {
+        String orderId = "123";
+        String userEmail = "test@example.com";
+
+        Order order = new Order(orderId, userEmail);
+
+        assertEquals(orderId, order.getOrderId());
+        assertEquals(userEmail, order.getUserEmail());
+    }
+
+    @Test
+    public void testGetOrderId() {
+        String orderId = "123";
+        String userEmail = "test@example.com";
+
+        Order order = new Order(orderId, userEmail);
+
+        assertEquals(orderId, order.getOrderId());
+    }
+
+    @Test
+    public void testSetOrderId() {
+        String orderId = "123";
+        String newOrderId = "456";
+        String userEmail = "test@example.com";
+
+        Order order = new Order(orderId, userEmail);
+
+        order.setOrderId(newOrderId);
+
+        assertEquals(newOrderId, order.getOrderId());
+    }
+
+    @Test
+    public void testGetUserEmail() {
+        String orderId = "123";
+        String userEmail = "test@example.com";
+
+        Order order = new Order(orderId, userEmail);
+
+        assertEquals(userEmail, order.getUserEmail());
+    }
+
+    @Test
+    public void testSetUserEmail() {
+        String orderId = "123";
+        String userEmail = "test@example.com";
+        String newUserEmail = "newtest@example.com";
+
+        Order order = new Order(orderId, userEmail);
+
+        order.setUserEmail(newUserEmail);
+
+        assertEquals(newUserEmail, order.getUserEmail());
+    }
+}
