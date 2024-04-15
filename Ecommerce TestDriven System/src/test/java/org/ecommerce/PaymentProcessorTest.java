47448c5df7a212ec0930d4a764a4a6c898ea4cb8
youssefalbertt <youssefalbert2003@gmail.com>
2024-04-15 17:14:39 +0200

    add payment processor test file

---
+++ Ecommerce TestDriven System/src/test/java/org/ecommerce/PaymentProcessorTest.java
@@ -0,0 +1,25 @@
+package org.ecommerce;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentProcessorTest {
+    @Test
+    public void testProcessPaymentWithValidAmount() {
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        assertTrue(paymentProcessor.processPayment(100.0));
+    }
+
+    @Test
+    public void testProcessPaymentWithZeroAmount() {
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        assertFalse(paymentProcessor.processPayment(0.0));
+    }
+
+    @Test
+    public void testProcessPaymentWithNegativeAmount() {
+        PaymentProcessor paymentProcessor = new PaymentProcessor();
+        assertFalse(paymentProcessor.processPayment(-100.0));
+    }
+}
