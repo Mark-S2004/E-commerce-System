0f1071b5c0e157bebdafcd4bfbd379071b7de28d
Geno212 <ahmed8811223344@gmail.com>
2024-04-17 16:13:59 +0200

    -update all classes

---
+++ Ecommerce TestDriven System/src/main/java/org/ecommerce/OrderManagement.java
@@ -0,0 +1,24 @@
+package org.ecommerce;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderManagement {
+    private List<Orders> orders;
+
+    public OrderManagement() {
+        this.orders = new ArrayList<>();
+    }
+
+    public void placeOrder(Orders order) {
+        orders.add(order);
+    }
+
+    public void cancelOrder(Orders order) {
+        orders.remove(order);
+    }
+
+    public List<Orders> getAllOrders() {
+        return orders;
+    }
+}
