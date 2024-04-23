286f126e5c53f53d5e88be1d38135fa9d80c2f89
Mark Saleh <mark-saleh@outlook.com>
2024-04-23 03:39:25 +0200

    Develop System Test Suite

---
+++ Ecommerce System GUI/src/test/java/EcommerceSystem/SystemSuiteTest.java
@@ -0,0 +1,20 @@
+package EcommerceSystem;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("System Test Suite")
+@SelectClasses({
+        ProductTest.class,
+        ProductCatalogTest.class,
+        ShoppingCartTest.class,
+        PaymentProcessorTest.class,
+        OrderTest.class,
+        OrderManagementTest.class,
+        AccountTest.class,
+        AccountManagerTest.class
+})
+public class SystemSuiteTest {
+}
