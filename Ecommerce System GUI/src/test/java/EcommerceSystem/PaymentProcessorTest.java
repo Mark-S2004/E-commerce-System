342f582a5fcae2b2966fad4eddb5c9334db58cf6
Mark Saleh <mark-saleh@outlook.com>
2024-04-24 16:03:51 +0200

    Cover all branches and test all scenarios for PaymentProcessor

--- Ecommerce System GUI/src/test/java/EcommerceSystem/PaymentProcessorTest.java
+++ Ecommerce System GUI/src/test/java/EcommerceSystem/PaymentProcessorTest.java
@@ -1,25 +1,49 @@
 package EcommerceSystem;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PaymentProcessorTest {
+    private PaymentProcessor paymentProcessor;
+
+    @BeforeEach
+    void setup() {
+        paymentProcessor = new PaymentProcessor();
+    }
+
     @Test
     public void testProcessPaymentWithValidAmount() {
-        PaymentProcessor paymentProcessor = new PaymentProcessor();
         assertTrue(paymentProcessor.processPayment(100.0));
     }
 
     @Test
     public void testProcessPaymentWithZeroAmount() {
-        PaymentProcessor paymentProcessor = new PaymentProcessor();
         assertFalse(paymentProcessor.processPayment(0.0));
     }
 
     @Test
     public void testProcessPaymentWithNegativeAmount() {
-        PaymentProcessor paymentProcessor = new PaymentProcessor();
         assertFalse(paymentProcessor.processPayment(-100.0));
+    }
+
+    @Test
+    public void testDisconnectGatewayProcessPayment() {
+        paymentProcessor.disconnectPaymentGateway();
+        assertFalse(paymentProcessor.processPayment(-100.0));
+    }
+
+    @Test
+    void connectPaymentGateway() {
+        paymentProcessor.connectPaymentGateway();
+        assertTrue(paymentProcessor.isPaymentGatewayConnected());
+    }
+
+    @Test
+    void disconnectPaymentGateway() {
+        paymentProcessor.disconnectPaymentGateway();
+        assertFalse(paymentProcessor.isPaymentGatewayConnected());
     }
 }
