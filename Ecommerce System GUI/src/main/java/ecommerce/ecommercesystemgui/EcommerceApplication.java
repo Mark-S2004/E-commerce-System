9ab37b2d3cbd0a2c365f3c4a8f3d3f426a5aed3a
Mark Saleh <mark-saleh@outlook.com>
2024-04-25 11:51:46 +0200

    Add application icon

--- Ecommerce System GUI/src/main/java/ecommerce/ecommercesystemgui/EcommerceApplication.java
+++ Ecommerce System GUI/src/main/java/ecommerce/ecommercesystemgui/EcommerceApplication.java
@@ -3,9 +3,11 @@ package ecommerce.ecommercesystemgui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class EcommerceApplication extends Application {
     @Override
@@ -13,13 +15,12 @@ public class EcommerceApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(EcommerceApplication.class.getResource("start.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Homepage");
+        stage.getIcons().add(new Image(Objects.requireNonNull(EcommerceApplication.class.getResourceAsStream("images/shopping.png"))));
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        //AccountManager a=new AccountManager();
-
         launch();
     }
 }
