7ad402938222d973d74a00ff2cc0ee38b2066745
Geno212 <ahmed8811223344@gmail.com>
2024-04-25 23:07:47 +0200

    fix adding same product multiple times

--- Ecommerce System GUI/src/main/java/ecommerce/ecommercesystemgui/AddProductsController.java
+++ Ecommerce System GUI/src/main/java/ecommerce/ecommercesystemgui/AddProductsController.java
@@ -19,12 +19,15 @@ public class AddProductsController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    Boolean duplicate=false;
     @FXML
     private ListView<Product> catalogList;
     @FXML
     private TextField idField, nameField, priceField;
     @FXML
     private Label errorLabel;
+    @FXML
+    private Label errorLabel2;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -45,29 +48,41 @@ public class AddProductsController implements Initializable {
         String name = nameField.getText();
         String priceText = priceField.getText();
         String id = idField.getText();
-
-        try {
-            double price = Double.parseDouble(priceText);
-            Product product = new Product(id, name, price);
-            ProductCatalog.addProduct(product);
-            catalogList.getItems().add(product);
-            catalogList.setCellFactory(param -> new ListCell<Product>() {
-                @Override
-                protected void updateItem(Product product, boolean empty) {
-                    super.updateItem(product, empty);
-                    if (empty || product == null)
-                        setText(null);
-                    else
-                        setText(product.getName() + " - $" + product.getPrice());
-                }
-            });
-            nameField.clear();
-            idField.clear();
-            priceField.clear();
-            System.out.println("Product added successfully");
-        } catch (NumberFormatException ex) {
-            errorLabel.setVisible(true);
+        for(Product productloop:ProductCatalog.getAllProducts()) {
+            if (name.equals(productloop.getName())) {
+                duplicate = true;
+                break;
+            }
         }
+        if (duplicate)
+            errorLabel2.setVisible(true);
+
+        else {
+            try {
+                double price = Double.parseDouble(priceText);
+                Product product = new Product(id, name, price);
+                ProductCatalog.addProduct(product);
+                catalogList.getItems().add(product);
+                catalogList.setCellFactory(param -> new ListCell<Product>() {
+                    @Override
+                    protected void updateItem(Product product, boolean empty) {
+                        super.updateItem(product, empty);
+                        if (empty || product == null)
+                            setText(null);
+                        else
+                            setText(product.getName() + " - $" + product.getPrice());
+                    }
+                });
+                nameField.clear();
+                idField.clear();
+                priceField.clear();
+                System.out.println("Product added successfully");
+
+            } catch (NumberFormatException ex) {
+                errorLabel.setVisible(true);
+            }
+        }
+
     }
 
     @FXML
